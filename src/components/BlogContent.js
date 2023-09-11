41a4ab30b788e0dc0d36d7e623cedd22bd7ff3ec
walidbelharazem <walidbelharazem@gmail.com>
2023-09-11 18:02:02 +0200

    Completed assignment

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 function BlogContent(props) {
+  
   return <div id="blog-content">{props.articleText}</div>;
 }
+
 
 export default BlogContent;
--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -7,7 +7,7 @@ function BlogPost() {
     <div id="blog-post">
       <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
       <Comment commentText="I agree with this statement. - Angela Merkel" />
-      <Comment commentText="A universal truth. - Noam Chomsky" />
+      <Comment commentText="A universal truth. - Noam Chomsky" /> 
       <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
     </div>
   );
